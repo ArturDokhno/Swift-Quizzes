d87ed709bcaa4dad1d296b4ed91a19932e64ab7f
ArturDokhno <artur.dohno@icloud.com>
2021-12-19 17:07:16 +0500

    Initial Commit

---
+++ Swift-Quizzes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Quizzes
+//
+//  Created by Артур Дохно on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
